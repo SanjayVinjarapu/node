d98e4a8357f4f3c3649ff94984c99f6f379638c4
Sanjay Vinjarapu <sanjayvinjarapu36@gmail.com>
2020-08-21 12:55:14 +0530

    Adds speakers routing

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-
 const cookieSession = require('cookie-session');
 
 const FeedbackService = require('./services/FeedbackService');
@@ -13,6 +12,8 @@ const routes = require('./routes');
 
 const app = express();
 
+app.locals.siteName = 'ROUX Academy';
+
 const port = 3000;
 
 app.set('trust proxy', 1);
@@ -20,14 +21,22 @@ app.set('trust proxy', 1);
 app.use(
   cookieSession({
     name: 'session',
-    keys: ['abcdded', 'dasdasdasda'],
+    keys: ['Ghdur687399s7w', 'hhjjdf89s866799'],
   })
 );
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
+app.locals.siteName = 'ROUX Meetups';
+
 app.use(express.static(path.join(__dirname, './static')));
+
+app.use(async (request, response, next) => {
+  const names = await speakersService.getNames();
+  response.locals.speakerNames = names;
+  return next();
+});
 
 app.use(
   '/',
